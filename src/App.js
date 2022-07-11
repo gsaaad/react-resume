c42a4a53706493ec3a0ccf95a362f1e8472c2897
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-07-11 15:14:26 -0400

    started basic react portfolio project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header";
+function App() {
+  return (
+    <div className="App">
+      Welcome to the app
+      <Header></Header>
+      <section className="section">Section for content</section>
+      <footer className="footer">Footer</footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./index.css";
+function Header() {
+  return (
+    <div>
+      <header className="header">This is Header Component</header>;
+    </div>
+  );
+}
+
+export default Header;
