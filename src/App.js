c9b529f3f66180a604e134d1908d91a06d45d84c
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-07-12 20:43:57 -0400

    added some project starting component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Project from "./components/Project";
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <section className="section">Section for Projects</section>
+      <section className="section">
+        <h1>Projects</h1>
+        <Project></Project>
+      </section>
       <Footer></Footer>
     </div>
   );
