f263e92d6ad3c5a30115d98d4b71f3118846b1d4
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-07-12 20:34:24 -0400

    added footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <section className="section">Section for Projects</section>
-      <footer className="footer">Footer</footer>
+      <Footer></Footer>
     </div>
   );
 }
