f3aa6c62cd6687e0838309687afff6c000b26a9e
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-07-16 14:01:18 -0400

    added more details in Contact form and some css styling

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import "./index.css";
 function ContactMeForm() {
   const [errorMessage, setErrorMessage] = useState("");
   const [formState, setFormState] = useState({
@@ -8,12 +8,25 @@ function ContactMeForm() {
     message: "",
   });
   return (
-    <section>
+    <section className="Contact-form">
       <h1>Contact me</h1>
       <form>
-        <div>Name:</div>
-        <div>Email:</div>
-        <div>Message:</div>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input name="name" type="text"></input>
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input name="email" type="email"></input>
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea
+            name="message"
+            row="5"
+            placeholder="Send me a message of how I can help you! Will get back to you shortly."
+          ></textarea>
+        </div>
       </form>
     </section>
   );
