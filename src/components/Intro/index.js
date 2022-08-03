6603a506a3cb62ca28b7c06ceed8d7697807b06b
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-08-03 15:51:12 -0400

    added responsiveness for front page

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -3,20 +3,20 @@ import portfolioPhoto from "../../assets/img/Portfolio_Pic.jpg";
 import "./index.css";
 function Intro() {
   return (
-    <section className=" grid grid-rows-2 intro-container">
-      <div className="intro-self grid grid-cols-12">
+    <section className="grid lg:grid-cols-2">
+      <div className="intro-self ">
         <h1 className="intro-name">George Saad</h1>
         <h2 className="intro-title">Full Stack Developer</h2>
-        <p className="intro-descp">
+        <p className="intro-descp text-2xl  mt-8 lg:text-3xl lg:mr-32">
           Curious Full Stack Developer utilizing my background knowledge in
           Psychology and Neuroscience to build a more intuitive and enhanced
           user experience on applications.
         </p>
-        <p className="intro-descp2">
+        <p className="intro-descp2 text-2xl  mt-8 lg:text-3xl lg:mr-32">
           Expanding my skills in JavaScript, CSS, HTML, React, Node, and Python.
           A proven leader and effective team player.
         </p>
-        <p className="intro-descp3">
+        <p className="intro-descp3 text-2xl mt-8  lg:text-3xl lg:mr-32">
           Passionate and detail-oriented toward data analysis, web development,
           data science, and backend designs and data structures.
         </p>
