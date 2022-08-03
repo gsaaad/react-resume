1359b5bccbe5dd9bacb3661370317e5e5eb3881d
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-08-03 12:49:51 -0400

    touching up Intro section

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -3,17 +3,22 @@ import portfolioPhoto from "../../assets/img/Portfolio_Pic.jpg";
 import "./index.css";
 function Intro() {
   return (
-    <section className="Intro-container">
-      <div className="Intro-Self grid grid-cols-12">
-        <h1 className="Name">George Saad</h1>
-        <h2>Full Stack Developer</h2>
-        <p className="Intro-text ">
+    <section className=" grid grid-rows-2 intro-container">
+      <div className="intro-self grid grid-cols-12">
+        <h1 className="intro-name">George Saad</h1>
+        <h2 className="intro-title">Full Stack Developer</h2>
+        <p className="intro-descp">
           Curious Full Stack Developer utilizing my background knowledge in
           Psychology and Neuroscience to build a more intuitive and enhanced
-          user experience on applications. Expanding my skills in JavaScript,
-          CSS, HTML, React, Node, and Python. A proven leader and effective team
-          player. Passionate and detail-oriented toward data analysis, web
-          development, data science, and backend designs and data structures.
+          user experience on applications.
+        </p>
+        <p className="intro-descp2">
+          Expanding my skills in JavaScript, CSS, HTML, React, Node, and Python.
+          A proven leader and effective team player.
+        </p>
+        <p className="intro-descp3">
+          Passionate and detail-oriented toward data analysis, web development,
+          data science, and backend designs and data structures.
         </p>
       </div>
       <div>
